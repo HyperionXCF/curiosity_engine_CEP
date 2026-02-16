00203e54dd917dcc9064100ea54a2ef5d560cc85
Geeta1239 <geetakolte1239@gmail.com>
2026-02-16 20:53:33 +0530

    Added backend

--- backend/llm_service.py
+++ backend/llm_service.py
@@ -2,9 +2,12 @@ import json
 import os
 from fastapi import HTTPException
 from openai import OpenAI
+import requests # needed when we will use our own endpoint
 
 # Groq API key
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# CURIOSITY_ENGINE_URL = "http://localhost:9000/generate"  # endpoint by vipul.
 
 # Initialize Groq client
 client = OpenAI(
@@ -18,6 +21,18 @@ def send_prompt_to_llm(prompt: str) -> dict:
     and returns structured response.
     """
     try:
+
+        # response = requests.post(
+        #     CURIOSITY_ENGINE_URL,
+        #     json={"question": prompt}
+        # )
+
+        # if response.status_code != 200:
+        #     raise HTTPException(
+        #         status_code=500,
+        #         detail="Curiosity Engine failed"
+        #     )
+        
         response = client.chat.completions.create(
             model="llama-3.3-70b-versatile",
             messages=[
